9b4ae194587ce6bdb90b1f8d44d2770d70ef5614
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-06-14 10:43:33 -0500

    from >4s to ~.8s

--- names/names.py
+++ names/names.py
@@ -10,11 +10,16 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+# 4.4s -> .8s
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+for name_2 in names_2:
+	if name_2 in names_1:
+		duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
